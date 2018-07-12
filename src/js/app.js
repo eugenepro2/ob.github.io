a7f790f588d99511786893cb73b48c45123e0516
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-07-12 16:54:00 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,8 @@
 import select from './lib/selectize.min';
 import './import/swiper';
 import './import/ui';
+import objectFitImages from 'object-fit-images';
+
+
+
+objectFitImages();
--- src/js/import/swiper.js
+++ src/js/import/swiper.js
@@ -7,3 +7,26 @@ var swiper = new Swiper('.swiper-intro', {
     prevEl: '.swiper-intro-prev',
   },
 });
+
+
+//catalog-rooms
+
+var swiper = new Swiper('.swiper-rooms', {
+  direction: 'vertical',
+  pagination: {
+    el: '.swiper-pagination-rooms',
+    clickable: true,
+  },
+});
+//room
+
+var swiper = new Swiper('.swiper-rooms-card', {
+  navigation: {
+    nextEl: '.swiper-rooms-card-next',
+    prevEl: '.swiper-rooms-card-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination-rooms-card',
+    clickable: true,
+  },
+});
