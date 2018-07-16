c66e4c44f3597ab42c0a6ee606aeeff49ede3ce9
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-07-16 11:18:15 +0300

    fix

--- src/js/import/ui.js
+++ src/js/import/ui.js
@@ -8,3 +8,14 @@ $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
     .addClass('active').siblings().removeClass('active')
     .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
 });
+
+//menu mobile
+$('.menu-open').on('click', function() {
+  $('.menu').addClass('active');
+  $('.pop-up-bg').fadeIn();
+});
+
+$('.menu-close, .pop-up-bg').on('click', function() {
+  $('.menu').removeClass('active');
+  $('.pop-up-bg').fadeOut();
+});
